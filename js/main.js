75c720c3f47c13d4f385685f17478f26e929f439
leehj04271 <122263151+leehj04271@users.noreply.github.com>
2023-03-14 17:41:14 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -94,8 +94,8 @@ const getHtmlText = () => {
 
 
         const htmlText =
-            `<div class="subway-map" data-columns="12" data-rows="20" data-cellSize="50" data-legendId="legend"
-data-textClass="text" data-gridNumbers="true" data-grid="false" data-lineWidth="8">
+    `<div class="subway-map" data-columns="15" data-rows="24" data-cellSize="50" data-legendId="legend"
+data-textClass="text" data-gridNumbers="true" data-grid="true" data-lineWidth="8">
 
 <ul data-color="#82eca7" data-label="jQuery Widgets">
 ${lisMobile.join('')}
