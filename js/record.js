209133a7317b50ec9832f18c23a216d7566ea855
leehj04271 <122263151+leehj04271@users.noreply.github.com>
2023-03-15 11:25:21 +0900

    Update record.js

--- js/record.js
+++ js/record.js
@@ -1,29 +1,40 @@
-(function($){
+(function ($) {
 
+    console.log(localStorage)
     if (localStorage.recentSearch) {
-        gdata = JSON.parse(localStorage.recentSearch)
-        if(recentSearch){
-            id = recentSearch[recentSearch.length-1].id
-        }
-        usedata(recentSearch)
-    }
-    
-    $('#star').on('click', '.addS',function(){
-        let aname = $(this).attr('data-statn')
-        let obj = { id:id, text:aname }
-        recentSearch.push(obj)
-        localStorage.recentSearch = JSON.stringify(gdata)
-        usedata(recentSearch)
-    })
+        const gdata = JSON.parse(localStorage.recentSearch)
 
-    function usedata(rdata){
+        usedata(gdata.reverse())
+    }
+
+
+    function usedata(rdata) {
         $('#record .rec div').remove()
         let list = `<div class="cord">`
-        rdata.map(function(value){
-            list += `<a href="./trainInfo?statn_nm=${value.text}">${value.text}</a>`
+        rdata.map(function (value) {
+            list += `
+<div>
+<a href="./trainInfo?statn_nm=${value.text}">${value.text}</a>
+</div>`
         })
         list += `</div>`
         $('#record .re').before(list)
     }
 
+
+    $('#wrap').on('click', '#record a', function (e) {
+
+        e.preventDefault()
+        const text = $(this).text();
+
+
+        var state = {statn_nm: text};
+        history.pushState(state, null);
+
+        $('#wrap').removeClass()
+        $('#wrap').addClass('trainInfo')
+
+        $("#wrap").load(`trainInfo.html`);
+
+    })
 })(jQuery)
